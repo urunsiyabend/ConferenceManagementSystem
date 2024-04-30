8535d0cb00ea1ac4de8fd96e9a04758f576ae765
burakdegirmencio <burak.degirmencioglu@hotmail.com>
2024-04-30 23:07:42 +0300

    dümenden commit

--- src/test/java/com/urunsiyabend/conferencemanagementsystem/ConferenceManagementSystemApplicationTests.java
+++ src/test/java/com/urunsiyabend/conferencemanagementsystem/ConferenceManagementSystemApplicationTests.java
@@ -3,11 +3,14 @@ package com.urunsiyabend.conferencemanagementsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ConferenceManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1,1);
     }
 
 }
